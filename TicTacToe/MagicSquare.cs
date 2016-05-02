3a309871bf7f0263a9dbb276901a353f5f0a229e
Sebastian Gralla <sebastian.gralla@gmail.com>
2016-05-02 20:11:45 +0200

    Revert "Update MagicSquare ToDo: Lösung entwickeln sobald k > n ist"
    This reverts commit 5ede57ebca0cf635725544237fa628dfad74c01e.

--- TicTacToe/MagicSquare.cs
+++ TicTacToe/MagicSquare.cs
@@ -18,24 +18,14 @@ namespace TicTacToe
 
             // initiaze
             int[,] quadrat = new int[n, n];
-
-            // kann durch k ersetzt werden
-            // int number = 1;
-            // keine initialisierung erforderlich
-            //int i = 0;
-            //int j = 0;
-            int i, j, ia, ja, ib, jb;
-
-            //
-            ia = 1; //1; 
-            ja = 2; // 0;
-            // 9 = 2,1
-            // 1 = 0,1
-            // 2 = 2,0
-            // 
-
-            ib = 1; //1; // 0
-            jb = 0; //2; // 1
+            int number = 1;
+            int i = 0;
+            int j = 0;
+            int ia, ja, ib, jb;
+            ia = 0;
+            ja = 0;
+            ib = 0;
+            jb = 1;
             bool turn = true;
 
             //ia = 
@@ -44,33 +34,24 @@ namespace TicTacToe
             // mitte
             quadrat[n / 2, n / 2] = (n * n + 1) / 2;
 
-            for (int k = 1; k < (n*n); k++)
+            for (int k = 0; k < (n*n); k++)
             {
-                // anstatt auf 2,2 für 4 lande ich bei 0,1 wodurch die 1 dort überschrieben wird
-                // die 6 überschreibt zwar dann die 9 auf der 2,1 ist aber dem Muster entsprechend richtig
+                // ich fange bereits mit der 1 position an
                 i = ib - ia;
                 j = jb - ja;
-                ia = ib; 
+                ia = ib;
                 ja = jb;
 
-                if (k/2+1 > n+1)
-                {
-                    i = n-1;
-                    j = n-1;
-                }
                 i = i < 0 ? i += n : i = i;
                 j = j < 0 ? j += n : j = j;
                  
                 ib = i;
                 jb = j;
                 if (turn)
-                    quadrat[i, j] = k/2+1;
-                else
-                    quadrat[i, j] = (n * n + 1) - (k/2);
-                 //   quadrat[i, j] = (n * n) - (k / 2);
-                 
-                
-
+                    quadrat[i, j] = number;
+                else 
+                    quadrat[i, j] = (n * n + 1) - number;
+                number++;
                 turn = !turn;
 
                 Console.WriteLine(quadrat[i, j]);
